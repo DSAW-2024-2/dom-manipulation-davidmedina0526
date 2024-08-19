976185df8a4c016c45ea1c62295ffc2b3f90b6f4
davidmedina0526 <davidmebu@unisabana.edu.co>
2024-08-19 00:12:36 -0500

    Entrega taller manipulación DOM

---
+++ app.js
@@ -0,0 +1,54 @@
+window.onload = function () {
+    
+    const imagenes = [
+        'assets/image_1.jpg',
+        'assets/image_2.jpeg',
+        'assets/image_3.jpeg'
+    ];
+
+    let posicionActual = 0;
+    let botonAdelante = document.querySelector('.Adelante');
+    let botonAtras = document.querySelector('.Atras');
+    let imagen = document.querySelector('.Imagen');
+
+    function pasarFoto() {
+        if(posicionActual >= imagenes.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        actualizarImagen();
+    }
+
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = imagenes.length - 1;
+        } else {
+            posicionActual--;
+        }
+        actualizarImagen();
+    }
+
+    function actualizarImagen () {
+        imagen.style.backgroundImage = `url(${imagenes[posicionActual]})`;
+        botonAdelante.disabled = posicionActual === imagenes.length - 1;
+        botonAtras.disabled = posicionActual === 0;
+
+        if (posicionActual === 0) {
+            botonAtras.style.display = 'none';
+        } else {
+            botonAtras.style.display = 'block'; //
+        }
+    
+        if (posicionActual === imagenes.length - 1) {
+            botonAdelante.style.display = 'none';
+        } else {
+            botonAdelante.style.display = 'block'; 
+        }
+    }
+
+    botonAdelante.addEventListener('click', pasarFoto);
+    botonAtras.addEventListener('click', retrocederFoto);
+
+    actualizarImagen();
+}
